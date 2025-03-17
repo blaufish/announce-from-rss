723cd8278640dc37c314002ca5669959be0a8167
blaufish <blaufish@users.noreply.github.com>
2025-03-17 15:22:10 +0100

    Squash twitter-announce-from-rss/master
    2600199 Initial draft code
    0003816 Cleanup
    ac00dd0 Add a test-tweet feature
    3e20c8c Check RSS first, exit early if possible
    d765612 Implement tweeting :)
    8b931c0 Virtual environment
    2b405e6 Documentation + spellchecker
    4caf445 Lets memorialize this little test snippet
    6f82915 Lets remove the test snippet
    e51817a git repo setup: .gitattributes .gitignore

---
+++ spellcheck.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+spellcheck() {
+	local f="$1"
+	echo "Spellcheck $f"
+	aspell check --mode=markdown --lang=en_us -- "$f"
+}
+
+which aspell >/dev/null
+if [[ "$?" != "0" ]]
+then
+	echo "No aspell? Maybe:"
+	echo "sudo apt install aspell aspell-en"
+	exit 1
+fi
+
+spellcheck README.md
